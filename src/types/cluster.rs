4e320e9a647d5efbcd60873c1bf6f5b6cfbfc3a9
Xavier Lange <xrlange@gmail.com>
2021-12-06 17:33:46 -0500

    make pub

--- src/types/cluster.rs
+++ src/types/cluster.rs
@@ -3,32 +3,32 @@ use std::collections::HashMap;
 #[derive(Debug, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Cluster {
-    actions: HashMap<String, String>,
-    agent_features: HashMap<String, bool>,
-    agent_image: String,
-    agent_image_override: String,
-    aks_status: HashMap<String, Option<String>>,
-    allocatable: HashMap<String, String>,
-    annotations: HashMap<String, String>,
+    pub actions: HashMap<String, String>,
+    pub agent_features: HashMap<String, bool>,
+    pub agent_image: String,
+    pub agent_image_override: String,
+    pub aks_status: HashMap<String, Option<String>>,
+    pub allocatable: HashMap<String, String>,
+    pub annotations: HashMap<String, String>,
     // answers:
-    api_endpoint: String,
-    applied_enable_network_policy: bool,
-    applied_pod_security_policy_template_id: String,
+    pub api_endpoint: String,
+    pub applied_enable_network_policy: bool,
+    pub applied_pod_security_policy_template_id: String,
     // applied_spec:
-    auth_image: String,
-    base_type: String,
-    ca_cert: String,
+    pub auth_image: String,
+    pub base_type: String,
+    pub ca_cert: String,
     // capabilities:
-    capacity: HashMap<String, String>,
+    pub capacity: HashMap<String, String>,
     // certificates_expiration:
-    cluster_template_id: String,
-    cluster_template_revision_id: String,
+    pub cluster_template_id: String,
+    pub cluster_template_revision_id: String,
     // component_statuses:
-    created: String,
+    pub created: String,
     #[serde(rename = "createdTS")]
-    created_ts: i64,
-    creator_id: String,
-    id: String,
-    labels: HashMap<String, String>,
-    name: String,
+    pub created_ts: i64,
+    pub creator_id: String,
+    pub id: String,
+    pub labels: HashMap<String, String>,
+    pub name: String,
 }
